adcc43ad71fc3b538bfc82a7e72fc11163606ea8
vasilievgip <vasilievgip@yandex.ru>
2022-12-20 16:57:34 +0300

    оформление

--- Documents/SecondViewController.swift
+++ Documents/SecondViewController.swift
@@ -9,15 +9,15 @@ import UIKit
 
 
 class SecondViewController: UIViewController {
-
+    
     weak var coordinator: MainSecondCoordinator?
-
-
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = .white
         self.tabBarItem = UITabBarItem(title: "Second", image: UIImage(systemName: "square"), tag: 1)
     }
-
+    
 }
 
