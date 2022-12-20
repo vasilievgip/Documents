ad4e96983f5e363692da29b8a7751a14c72ae6aa
vasilievgip <vasilievgip@yandex.ru>
2022-12-20 10:27:49 +0300

    Initial Commit

---
+++ Documents/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Documents
+//
+//  Created by Андрей Васильев on 20.12.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
